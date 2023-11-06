90e7ab58629de9960e9bb690fa8615ba93496a2a
DRV111 <xblackcrownx@gmail.com>
2023-11-06 16:06:12 +0300

    feat: render balance text style conditionally

--- src/components/FriendProfile.jsx
+++ src/components/FriendProfile.jsx
@@ -3,7 +3,17 @@ function FriendProfile({ friend }) {
     <li>
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
-      <p>{friend.balance}</p>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${friend.balance}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You are and {friend.name} even</p>}
       <button className="button">Select</button>
     </li>
   );
