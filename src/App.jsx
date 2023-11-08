09707d98cd74e41e450c3f9a785a76b6e305bd36
DRV111 <xblackcrownx@gmail.com>
2023-11-08 08:21:47 +0300

    feat: add addFriend component and open and close form functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import { useState } from 'react';
 import AddFriend from './components/AddFriend';
 import FriendList from './components/FriendList';
 import SplitBillForm from './components/SplitBillForm';
 import './index.css';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  function handleOpen() {
+    if (isOpen) {
+      setIsOpen(false);
+    }
+  }
   return (
     <div className="app">
       <div className="sidebar">
         <FriendList />
-        <AddFriend />
+
+        {!isOpen ? (
+          <button onClick={() => setIsOpen(true)} className="button">
+            Add friend
+          </button>
+        ) : (
+          <AddFriend handleOpen={handleOpen} />
+        )}
       </div>
       <SplitBillForm />
     </div>
--- src/components/AddFriend.jsx
+++ src/components/AddFriend.jsx
@@ -1,9 +1,18 @@
-function AddFriend() {
-    return (
-        <div>
-            Add Friend
-        </div>
-    )
+function AddFriend({ handleOpen }) {
+  return (
+    <div>
+      <form className="form-add-friend">
+        <label>👥Friend name</label>
+        <input type="text" />
+        <label>🖼Image URL</label>
+        <input type="text" />
+        <button className="button">Add friend</button>
+      </form>
+      <button onClick={() => handleOpen(false)} className="button">
+        Close
+      </button>
+    </div>
+  );
 }
 
-export default AddFriend
+export default AddFriend;
